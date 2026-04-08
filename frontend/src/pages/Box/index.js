54937687657adab7c57e74c5f099d49e78e7619e
Paulo Henrique Pfitzner <cientistaphp@gmail.com>
2026-04-08 14:43:58 -0300

    Atualização em tempo Real ao soltar os arquivos

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -5,7 +5,6 @@ import { ptBR } from 'date-fns/locale';
 import Dropzone from 'react-dropzone';
 import { io } from "socket.io-client";
 
-
 import { MdInsertDriveFile } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
 
@@ -16,44 +15,66 @@ const socket = io("https://cloudbox-production-d43f.up.railway.app", {
   transports: ["websocket"],
 });
 
-
 class Box extends Component { 
   state = { box: {} };
 
   async componentDidMount() {
-     this.subscribeToNewFiles();
+    this.subscribeToNewFiles();
 
     const box = this.props.params.id;
 
     const response = await api.get(`boxes/${box}`);
     this.setState({ box: response.data });
   }
- 
+
   subscribeToNewFiles = () => {
-  const box = this.props.params.id;
+    const box = this.props.params.id;
 
-  socket.emit('connectRoom', box);
+    socket.emit('connectRoom', box);
 
-  socket.on('file', data => {
-               this.setState({
-                  box: {
-                     ...this.state.box,
-                   files: [data, ...(this.state.box.files || [])],
-                  },
-              });
-       });
-};
+    socket.on('file', data => {
+      this.setState(prevState => {
+        // 🔥 evita duplicação
+        const alreadyExists = prevState.box.files?.some(
+          file => file._id === data._id
+        );
 
-  handleUpload = (files)=>{
-            files.forEach(file => {
-              const data = new FormData();
-              const box = this.props.params.id;
-  
-              data.append('file',file);
-  
-              api.post(`boxes/${box}/files`, data);
-  });
-};
+        if (alreadyExists) return prevState;
+
+        return {
+          box: {
+            ...prevState.box,
+            files: [data, ...(prevState.box.files || [])],
+          },
+        };
+      });
+    });
+  };
+
+  handleUpload = async (files) => {
+    const box = this.props.params.id;
+
+    for (const file of files) {
+      const data = new FormData();
+      data.append('file', file);
+
+      try {
+        // 🔥 aguarda resposta do backend
+        const response = await api.post(`boxes/${box}/files`, data);
+
+        // 🔥 atualização imediata (sem depender do socket)
+        this.setState(prevState => ({
+          box: {
+            ...prevState.box,
+            files: [response.data, ...(prevState.box.files || [])],
+          },
+        }));
+
+      } catch (err) {
+        console.error("Erro no upload:", err);
+      }
+    }
+  };
   
   render() {
     return (
@@ -64,28 +85,33 @@ class Box extends Component {
         </header>
 
         <Dropzone onDropAccepted={this.handleUpload}>
-            {({getRootProps,getInputProps })=>(
-                <div className="upload" {...getRootProps()}>
-                    <input {...getInputProps()} />
-      
-                    <p>Arraste arquivos ou clique aqui</ p>  
-                </ div>
-            )}
-        </ Dropzone>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Arraste arquivos ou clique aqui</p>  
+            </div>
+          )}
+        </Dropzone>
 
         <ul>
           {this.state.box.files && this.state.box.files.map(file => (
             <li key={file._id}>
-              <a className="fileinfo" href={file.url} target="_blank" rel="noreferrer">
+              <a 
+                className="fileinfo" 
+                href={file.url} 
+                target="_blank" 
+                rel="noreferrer"
+              >
                 <MdInsertDriveFile size={24} color="#A5CFFF" />
                 <strong>{file.title}</strong>
               </a>
+
               <span>
-                   há{" "}
-                   {formatDistanceToNow(new Date(file.createdAt), {
-                           locale: ptBR,
-                   })}
-                  </span>
+                há{" "}
+                {formatDistanceToNow(new Date(file.createdAt), {
+                  locale: ptBR,
+                })}
+              </span>
             </li>
           ))}
         </ul> 
